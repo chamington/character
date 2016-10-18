f7085c2cea927120f25e440333c86ae92ccf04a6
chamington <cosmictartar@gmail.com>
2016-10-18 16:18:55 -0400

    There

---
+++ compiler.py
@@ -0,0 +1,22 @@
+input="_________"
+bo=str(bin(len(input)))[2:]
+out=""
+for i in range (0,len(bo)//4):
+	x=bo[4*i]+bo[4*i+1]+bo[4*i+2]+bo[4*i+3]
+	if x=="1000":
+		out+=">"
+	if x=="1001":
+		out+="<"
+	if x=="1010":
+		out+="+"
+	if x=="1011":
+		out+="-"
+	if x=="1100":
+		out+="."
+	if x=="1101":
+		out+=","
+	if x=="1110":
+		out+="["
+	if x=="1111":
+		out+="]"
+print(out)	
---
+++ decompiler.py
@@ -0,0 +1,25 @@
+input="<"
+binout=""
+for i in range(0,len(input)):
+	if input[i]==">":
+		binout+="1000"
+	if input[i]=="<":
+		binout+="1001"
+	if input[i]=="+":
+		binout+="1010"
+	if input[i]=="-":
+		binout+="1011"
+	if input[i]==".":
+		binout+="1100"
+	if input[i]==",":
+		binout+="1101"
+	if input[i]=="[":
+		binout+="1110"
+	if input[i]=="]":
+		binout+="1111"
+print(int(binout,2))
+print("")
+
+for i in range(0, int(binout, 2)):
+	print("_",end="")
+
